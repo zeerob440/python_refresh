4efe68ab7a4fe255a3967b098a19bc2883edd080
MU-TH-ER <zeerob440@tutanota.com>
2026-05-15 12:32:57 -0400

    refresher.py| updated file

--- refresh.py
+++ refresh.py
@@ -1,1 +1,9 @@
 print('Git Bash Project Ready.')
+
+# if/else refresher example
+user: str = 'first'
+
+if user == 'first':
+    print ("Your're a winner!")
+else:
+    print ("If you aren't first, you're last!")
